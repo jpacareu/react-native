48077462bf640c3142ce9af34d943d412eb2e731
Javier <javierpacareu@gmail.com>
2018-08-15 21:03:08 -0300

    initial commit

---
+++ screens/LinksScreen.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { ScrollView, StyleSheet, View, Text } from 'react-native';
+import { ExpoLinksView } from '@expo/samples';
+
+export default class LinksScreen extends React.Component {
+  static navigationOptions = {
+    title: 'Links',
+  };
+
+  render() {
+    return (
+      <ScrollView style={styles.container}>
+        <View> <Text style={styles.getStartedText}>Get started by opening</Text></View>
+       </ScrollView>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingTop: 15,
+    backgroundColor: '#fff',
+  },
+});
